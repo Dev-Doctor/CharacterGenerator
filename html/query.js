7caaf0be4fd26f170bc943d1d678e8ab8fa35bb7
Fuma <andrew.fuma200304@gmail.com>
2022-11-06 17:45:02 +0100

    Versione migliorata della home page

--- html/query.js
+++ html/query.js
@@ -1,3 +1,11 @@
 $("#getStarted").click(function pageRedirect() {
     window.location.replace("/generator.html");
 })
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
